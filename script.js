129297730681bad3af74cf74940199c388cccc77
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-02-12 13:52:56 -0700

    added password generating script

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ function handleCopy() {
         alert('Password copied to clipboard!');
     });
 }
+
+function generatePassword() {
+    const length = 24;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$&*()_+':;?,./-";
+
+    let password = "";
+
+    const indexValues = new Uint8Array(length);
+    window.crypto.getRandomValues(indexValues);
+    
+    for (let i = 0; i < length; i++) {
+        password += charset[indexValues[i] % charset.length];
+    }
+
+    document.getElementById('password-display').value = password;
+}
